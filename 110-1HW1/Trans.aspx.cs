79733622a82d4c1f59ef00335671083a9f34780f
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-01 13:30:53 +0800

    新增Trans

---
+++ 110-1HW1/Trans.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double d_Value = 3.982;
+
+            double d_Value2 = (d_Value* d_Value)*0.3025;
+
+            Response.Write("d_Value " + "= 3.982 * 3.982 " + " = " + d_Value2);
+        }
+    }
+}
